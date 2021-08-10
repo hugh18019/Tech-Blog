16e709b2fd98eb994bec8a1dd2da87b5f926c283
Huiran Lin <hugh80082@gmail.com>
2021-08-10 02:37:37 -0700

    Add logic to display login/logout button

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
-const path = require('path');
-const express = require('express');
-const exphbs = require('express-handlebars');
+const path = require('path'); //guide express.static to public folder
+const express = require('express'); //express server
+const session = require('express-session'); //import express-sessions
+const routes = require('./controllers');
+const exphbs = require('express-handlebars'); //import express-handlebars
+// import sequelize connection
+const sequelize = require('./config/connection'); //import connection to db
+const SequelizeStore = require('connect-session-sequelize')(session.Store); //storing sessions
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// import sequelize connection
-const sequelize = require('./config/connection');
+const sess = {
+  secret: 'Super secret secret',
+  cookie: { maxAge: 1000 * 60 * 10 }, //cookie timer is 10 minutes (60000 ms)
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
 
 const hbs = exphbs.create({});
 
@@ -17,7 +31,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(require('./controllers/'));
+app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
 sequelize.sync({ force: false }).then(() => {
