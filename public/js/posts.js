e3cb44ed56239a6e2443ca8f7bcf49c7a145a886
Huiran Lin <hugh80082@gmail.com>
2021-08-13 23:32:12 -0700

    Add event listeners to create area for new posts

--- public/js/posts.js
+++ public/js/posts.js
@@ -2,13 +2,19 @@ var newPostBtn = document.querySelector('#new-post-btn');
 var newPostArea = document.querySelector('#new-post');
 
 function handleNewPostBtn() {
-  newPostArea.hidden = false;
+  if (newPostArea.hidden === false) {
+    newPostArea.hidden = true;
+    console.log(newPostArea.hidden);
+  } else {
+    newPostArea.hidden = false;
+    console.log(newPostArea.hidden);
+  }
 }
 
 newPostBtn.addEventListener('click', handleNewPostBtn);
 
 document.addEventListener('mouseup', function (e) {
-  if (!newPostArea.contains(e.target)) {
+  if (!newPostArea.contains(e.target) && !newPostBtn.contains(e.target)) {
     newPostArea.hidden = true;
   }
 });
