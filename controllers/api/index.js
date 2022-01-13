5175ce476c7445095ed00154e582adec185df6a6
hugh18019 <hugh80082@gmail.com>
2022-01-13 02:01:39 -0800

    Added login handlebars

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 
+const usersRoutes = require('./users-routes');
 const postsRoutes = require('./posts-routes');
 const commentRoutes = require('./comments-routes');
 const postDetailRoutes = require('./post-details-routes');
 
+router.use('/users', usersRoutes);
 router.use('/posts', postsRoutes);
 router.use('/post-details', postDetailRoutes);
 router.use('/comments', commentRoutes);
