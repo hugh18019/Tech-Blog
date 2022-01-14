856ba072eeeb4381f20df83ce4cae6ca332bd3a5
hugh18019 <hugh80082@gmail.com>
2022-01-14 02:20:57 -0800

    Modified connection.js for deploy

--- config/connection.js
+++ config/connection.js
@@ -10,15 +10,23 @@ Sequelize.DATE.prototype._stringify = function _stringify(date, options) {
   return date.format('YYYY-MM-DD HH:mm:ss.SSS');
 };
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306,
-  }
-);
+var sequelize;
+
+if ( process.env.JAWSDB_URL ) {
+  sequelize = new Sequelize( process.env.JAWSDB_URL );
+}
+else {
+
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306,
+    }
+  );
+}
 
 module.exports = sequelize;
